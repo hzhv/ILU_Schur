f3d804862c62cf55f929ba69aa50bf9a36a906df
hzhv <hzl0220@ucla.edu>
2025-10-07 17:35:01 -0400

    get 200 eigen vectors&values, reduce numerical error in min_res

--- min_res_sd.m
+++ min_res_sd.m
@@ -1,27 +1,21 @@
-% clc;clear;
-% 
-% n = 10;
-% rng("default")
-% A = rand(n);
-% rhs = (1:n)';
-% 
-% [x, iters, resvec] = min_res_sd(A, rhs, 1e-1, 50,[]);
-
 function [x, flag, relres, iters, resvec] = min_res_sd(a, b, tol, maxit, M1, M2)
 iters = 0;
 n = size(a,1);
 x = zeros(n,1);
 r = b - a*x;
 resvec = [norm(r)];
+
 if nargin < 6 || isempty(M2)
-    M2 = @(v)v;
+    M2 = @(v) v; 
 end
+
 while norm(r)/norm(b) > tol
-    kr = M2(r);
+    kr = M2(r);     % kr = precond * r
     p = a*kr;
     alpha = (p' * r) / (p' * p);
     x = x + kr*alpha;
-    r = r - p * alpha;
+    % r = r - p * alpha;
+    r = b - a*x;  % less numerical errors
     resvec = [resvec; norm(r)];
     iters = iters + 1;
     if iters >= maxit
--- test_MG.m
+++ test_MG.m
@@ -1,5 +1,5 @@
-%%
-clear;clc;close all;
+%%  k = [1, 2, 8, 16, 32];
+% clear;clc;close all;
 % n = 100;
 % rng("default")
 % A = rand(n);
@@ -10,18 +10,15 @@ A = load('./A_level2.mat').A;
 n = size(A,1);
 rhs = load('./rhs_level2.mat').x;
 rhs = rhs(:,1);
-% % k = [1, 2, 8, 16, 32];
+
 lb = {};
 index = 1;
-
-t=@(A,b)bicgstab(A,b,1e-1,100); % -> A^{-1}*x with a precision of 1e-1
+load("eigs200.mat");
 %%
-k = 1;
-[v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
-              'Tolerance',0.3,'MaxIterations',30);
-fprintf("Eigs done!!!\n")
-
+% k = 1;
+v = extractEigs(eigs200, 1);
 v = orth(v);
+
 tol_inner = 0.3; maxit_inner = 100;
 tol_outer = 1e-5; maxit_outer = 30;
 
@@ -30,33 +27,18 @@ tol_outer = 1e-5; maxit_outer = 30;
 plotMGD(inner_iter_vec, resvec_outer, 'min'); hold on;
 lb{index} = "min res, k=1";
 index = index + 1;
-
+%%
 [sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
     tol_inner, maxit_inner, tol_outer, maxit_outer, 1, 'bicgstab');
 plotMGD(inner_iter_vec, resvec_outer); hold on;
 lb{index} = "bicgstab, k=1";
 index = index + 1;
-
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "bicgstab unprec, k=1";
-index = index + 1;
-
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'min');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "min res unprec, k=1";
-index = index + 1;
-
-% =====================================================================================
-k = 2;
-[v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
-              'Tolerance',0.3,'MaxIterations',30);
-fprintf("Eigs done!!!\n")
+%% =====================================================================================
+% k = 2;
+v = extractEigs(eigs200, 2);
 v = orth(v);
 
-tol_inner = 0.3; maxit_inner = 100;
+tol_inner = 0.3; maxit_inner = 100; 
 tol_outer = 1e-5; maxit_outer = 30;
 
 [sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
@@ -74,19 +56,17 @@ index = index + 1;
 [sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
     tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
 semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "bicgstab unprec, k=2";
+lb{index} = "bicgstab unprec";
 index = index + 1;
 
 [sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
     tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'min');
 semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "min res unprec, k=2";
+lb{index} = "min res unprec";
 index = index + 1;
 % =====================================================================================
-k = 4;
-[v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
-              'Tolerance',0.3,'MaxIterations',30);
-fprintf("Eigs done!!!\n")
+% k = 4
+v = extractEigs(eigs200, 4);
 v = orth(v);
 
 tol_inner = 0.3; maxit_inner = 100;
@@ -104,22 +84,9 @@ plotMGD(inner_iter_vec, resvec_outer, 'bicgstab'); hold on;
 lb{index} = "bicgstab, k=4";
 index = index + 1;
 
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "bicgstab unprec, k=4";
-index = index + 1;
-
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "min res unprec, k=4";
-index = index + 1;
 %=====================================================================================
-k = 8;
-[v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
-              'Tolerance',0.3,'MaxIterations',30);
-fprintf("Eigs done!!!\n")
+% k = 8;
+v = extractEigs(eigs200, 8);
 v = orth(v);
 
 tol_inner = 0.3; maxit_inner = 100;
@@ -137,23 +104,9 @@ plotMGD(inner_iter_vec, resvec_outer, 'bicgstab'); hold on;
 lb{index} = "bicgstab, k=8";
 index = index + 1;
 
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "bicgstab unprec, k=8";
-index = index + 1;
-
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'min');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "min res unprec, k=8";
-index = index + 1;
-
 %=====================================================================================
-k = 16;
-[v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
-              'Tolerance',0.3,'MaxIterations',30);
-fprintf("Eigs done!!!\n")
+% k = 16;
+v = extractEigs(eigs200, 16);
 v = orth(v);
 
 tol_inner = 0.3; maxit_inner = 100;
@@ -171,23 +124,9 @@ plotMGD(inner_iter_vec, resvec_outer, 'bicgstab'); hold on;
 lb{index} = "bicgstab, k=16";
 index = index + 1;
 
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "bicgstab unprec, k=16";
-index = index + 1;
-
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'min');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "min res unprec, k=16";
-index = index + 1;
-
 %=====================================================================================
-k = 32;
-[v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
-              'Tolerance',0.3,'MaxIterations',30);
-fprintf("Eigs done!!!\n")
+% k = 32;
+v = extractEigs(eigs200, 32);
 v = orth(v);
 
 tol_inner = 0.3; maxit_inner = 100;
@@ -205,19 +144,18 @@ plotMGD(inner_iter_vec, resvec_outer, 'bicgstab'); hold on;
 lb{index} = "bicgstab, k=32";
 index = index + 1;
 
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'min');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "min res unprec, k=32";
-index = index + 1;
-
-[sol_x, res_inner, inner_iter_vec, resvec_outer, total_iters] = MG_deflation(A, rhs, v, ...
-    tol_inner, maxit_inner, tol_outer, maxit_outer, 0, 'bicgstab');
-semilogy(0:length(resvec_outer)-1, resvec_outer, '-o'); hold on;
-lb{index} = "bicgstab unprec, k=32";
-
 grid on;
 legend(lb);
+%%
+load("eigs200.mat");
+
+d_200 = sort(abs(diag(eigs200{2})), "descend");
+
+semilogy(d_200); grid on;
+title("Eigenvalue Magnitude Decay");
+xlabel("Magnitude Descend");
+ylabel("|Eig Val|")
+
 %%
 function plotMGD(inner_iter_vec, resvec_outer, solver)
     if nargin < 3 || isempty(solver) || strcmpi(solver, 'bicgstab')
@@ -229,4 +167,23 @@ function plotMGD(inner_iter_vec, resvec_outer, solver)
     assert(size(inner_iter_vec,1) == size(resvec_outer,1))
     X = cumsum(inner_iter_vec);
     semilogy(X, resvec_outer, LineStyle=m);
+end
+
+function [v, d] = getEigs(A, k, tol, maxit)
+    n = size(A, 1);
+    t=@(A,b)bicgstab(A,b,1e-1,100); % -> A^{-1}*x with a precision of 1e-1
+    [v, d] = eigs(@(x)t(A',t(A,x)), n, k, 'largestimag', ...
+              'Tolerance',tol,'MaxIterations',maxit);
+end
+
+function [v, d] = extractEigs(eigs, k)
+    if nargin < 2, k = 1; end
+    V = eigs{1};
+    D = eigs{2};
+    d = diag(D);
+    [~, perm] = sort(d, "descend"); % compare the real part for C
+    permD = D(perm, perm);
+    permV = V(:, perm); 
+    v = permV(:, 1:k);
+    d = permD(1:k, 1:k);
 end
