bd9a3efff44abcf3bdef15a48edaff8997b4bd0a
hike-underwood <tommone20@gmail.com>
2025-11-17 22:49:12 -0500

    Faster FSAI added

--- unsymBlockFSAI.m
+++ unsymBlockFSAI.m
@@ -5,7 +5,7 @@ function [precond, FL, FU, D] = unsymBlockFSAI(A, n_blocks)
 %   (x) precond 
 
     n = size(A, 1);    
-    S = spones(A) | spones(A');     % Symm the nnz pattern
+    S = spones(A) | spones(A');     % Symm the nnz pattern % TODO nnz inside our A's 64x64 blocks
     
     block_size = floor(n / n_blocks);
     limits = [1:block_size:n, n+1]; % Blocks start idx
@@ -18,71 +18,51 @@ function [precond, FL, FU, D] = unsymBlockFSAI(A, n_blocks)
         idx_end   = limits(kb+1) - 1;
         curr_cols = idx_start:idx_end;
         
-        M_ib = 1:(idx_start-1); % each r/c of Previous Blocks
-        
-        % Get FU and FL 
-        for k = 1:length(curr_cols)
-            idx = curr_cols(k);
-            
-            % get FU's idx col
-            g_U = find(S(M_ib, idx)); % nnz indices for f (upper)
-            g_L = g_U;                % nnz indices for f (left)
-            A_sub = A(g_L, g_U);
+        m_ib = 1:(idx_start-1);           % each r/c idx of Previous Blocks M_ib
+        S_block = S(m_ib, curr_cols);
+        g_U = m_ib(find(any(S_block,2))); % global row idx
+        % g_L = m_ib(find(any(S(curr_cols, m_ib),2)));
+        g_L = g_U;
 
-            if ~isempty(g_U)
-                % A_ib[gL, gU] * fU = -c  
-                rhs   = -A(g_L, idx);
-                
-                f_val = A_sub \ rhs; % test
-                
-                FU(g_U, idx) = f_val;
-            end
-            
-            % A(gU, gL)^T * fL = -r
-            if ~isempty(g_L)
-                A_sub_T = A_sub'; 
-                rhs_T   = -A(idx, g_U)';
-                
-                f_val = A_sub_T \ rhs_T;
-                
-                FL(idx, g_L) = f_val'; 
-            end
-        end
+        A_sub = A(g_U, g_L); % sparse
+        % % A(gL,gU) * f = -A(gU, idx)
+        rhs_U = -A(g_U, curr_cols);   
+        F_block_U = A_sub \ rhs_U;       
+        % mask_U = S(g_U, curr_cols);
+        % F_block_U(~mask_U) = 0;
+        FU(g_U, curr_cols) = F_block_U;
+        
+        % % A(gU, gL)^T * fL = -r'  ==>  fL' * A(gU, gL) = -r  
+        rhs_L = -A(curr_cols, g_L)';
+        F_block_L = (A_sub') \ rhs_L;
+        FL(curr_cols, g_L) = F_block_L';
+        
     end
     
-    M_approx = FL * A * FU;
+    M_approx = FL * A * FU; 
     
-    % Extract Block Diag B
-    D_inv_ops = cell(n_blocks, 1);
-    
-    D = sparse(n, n);    
+    D = sparse(n, n);
     for kb = 1:n_blocks
         idx = limits(kb):(limits(kb+1)-1);
         Block = M_approx(idx, idx);
-        D(idx, idx) = Block;
-        
-        [L_blk, U_blk] = lu(Block); 
-        D_inv_ops{kb}.L = L_blk;
-        D_inv_ops{kb}.U = U_blk;
+        D(idx, idx) = inv(Block);
     end
     
-    % M^{-1} = WU*WL = FU JU^{-1}JL^{-1} * FL = FU * D^{-1} * FL
-    % z = FL * r -> y = D^(-1) * z -> x = FU * y
-    precond = @(r) apply_solve(r, FL, FU, D_inv_ops, limits, n_blocks);
+    precond = @(r) FU * (D *(FL * r));
 end
 
-function x = apply_solve(r, FL, FU, D_inv_ops, limits, n_blocks)
-    z = FL * r;
+% function x = apply_solve(r, FL, FU, D_inv_ops, limits, n_blocks)
+%     z = FL * r;
     
-    % invert the bds (parallelizable)
-    y = zeros(size(z));
-    for kb = 1:n_blocks
-        idx = limits(kb):(limits(kb+1)-1);
-        % y(idx) = B_kb \ z(idx)
-        ops = D_inv_ops{kb};
-        block_rhs = ops.U \ (ops.L \ z(idx));
-        y(idx) = block_rhs;
-    end
+%     % invert the bds (parallelizable)
+%     y = zeros(size(z));
+%     for kb = 1:n_blocks
+%         idx = limits(kb):(limits(kb+1)-1);
+%         % y(idx) = B_kb \ z(idx)
+%         ops = D_inv_ops{kb};
+%         block_rhs = ops.U \ (ops.L \ z(idx));
+%         y(idx) = block_rhs;
+%     end
     
-    x = FU * y;
-end
+%     x = FU * y;
+% end
